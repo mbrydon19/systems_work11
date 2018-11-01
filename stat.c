1a775439f0cecb87b1e3c9fe262f51cd5b2c27ab
Faria Huq <fariahuq@Farias-MacBook-Air.local>
2018-11-01 14:31:49 -0400

    added

--- stat.c
+++ stat.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <time.h>
-
+//file size
+//mode (permissions)
+//time of last access (displayed in the following format: Fri Oct 28 09:03:01 2016
 int main(){
   struct stat info;
   stat( "file.txt", &info);
   printf( "file size: %lld\n", info.st_size);
-  printf( "permissions: %hu\n", info.st_mode);
+  printf( "permissions: %o\n", info.st_mode);
+  printf( "time of last access: %s\n", ctime(&info.st_atime));
 }
